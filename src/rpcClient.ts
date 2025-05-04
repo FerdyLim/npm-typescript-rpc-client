591e2847b0cca5bd331e8b22228f61c7cce03f35
Ferdinand Lim <limhjf@gmail.com>
2025-05-04 13:32:36 +0800

    Add handling for 404 error

--- src/rpcClient.ts
+++ src/rpcClient.ts
@@ -5,18 +5,28 @@ import { RpcHeaders, RpcRequestModel, RpcResponseModel } from "./types";
 dotenv.config();
 
 export async function callFunction(method: string, data: any, headers?: RpcHeaders) {
-    const request: RpcRequestModel = {
-        method,
-        data,
+    try {
+        const request: RpcRequestModel = {
+            method,
+            data,
+        }
+        const fullHeaders: AxiosRequestConfig = {
+            headers: {
+                // 'Authorization': "", // future enhancements
+                ...headers,
+                'Content-Type': 'application/json',
+            },
+            responseType: "json",
+        }
+        const response = await axios.post(process.env.RPC_URL+"/rpc",request, fullHeaders);
+        return response.data as RpcResponseModel;
+    } catch (error) {
+        return {
+            error: {
+                code: 404,
+                description: "Unable to reach server"
+            },
+            data: null,
+        } as RpcResponseModel;
     }
-    const fullHeaders: AxiosRequestConfig = {
-        headers: {
-            // 'Authorization': "", // future enhancements
-            ...headers,
-            'Content-Type': 'application/json',
-        },
-        responseType: "json",
-    }
-    const response = await axios.post(process.env.RPC_URL+"/rpc",request, fullHeaders);
-    return response.data as RpcResponseModel;
 }
